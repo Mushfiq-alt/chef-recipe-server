a19592055c635ded6e365175cf550dbaca57ce43
Mushfiq-alt <mushfiquzzaman.chowdhury@gmail.com>
2023-06-12 22:44:31 +0600

    food info updated

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefDetails = require("./data/chef.json");
+const popularFoods = require("./data/popularDish.json");
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef is running')
 });
+
+app.get("/chef", (req, res) => {
+    res.send(chefDetails);
+});
+
+app.get("/chef/:id", (req, res) => {
+    const id = req.params.id;
+
+    const selectedChef = chefDetails.find((n) => n.chef_id == id);
+    res.send(selectedChef);
+});
+
+app.get("/popular-foods", (req, res) => {
+    res.send(popularFoods);
+});
+
 
 app.listen(port, () => {
     console.log(`Chef API is running on PORT: ${port}`);
